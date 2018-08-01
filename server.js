05862f52ac34f5d2eb144bcbccc8311d9249ec02
zhoushoujian <qianchengyimeng@qq.com>
2018-08-01 23:15:09 +0800

    新增日志系统

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-require("colors");
-const fs = require("fs");
-const url = require('url');
-const path = require("path");
-const formidable = require('formidable');
-const http = require('http');
-const util = require('util');
-const cluster = require("cluster");
-const os = require("os");
-const child_process = require('child_process');
-const setting = require('./app/setting');
-const Render = require('./app/render');
-
 //以非主线程的方式运行
+let cluster = require("cluster");
 let IS_RUNNING_AS_SERVER = process.mainModule.IS_RUNNING_AS_SERVER = require.main === module;
 if (IS_RUNNING_AS_SERVER && cluster.isMaster) {
     return function () {
@@ -22,18 +10,30 @@ if (IS_RUNNING_AS_SERVER && cluster.isMaster) {
             worker = cluster.fork();
             worker.on('exit', (code, signal) => {
                 if (signal) {
-                    console.log('worker was killed by signl', signal);
+                    logger.debug('worker was killed by signl', signal);
                 } else if (code !== 0) {
-                    console.log("worker exited with error", code)
+                    logger.debug("worker exited with error", code)
                 } else {
-                    console.log("worker success!")
+                    logger.debug("worker success!")
                 }
             });
 
         }
-        work()
+        work();
     }();
 }
+
+require("colors");
+let fs = require("fs"),
+    url = require('url'),
+    path = require("path"),
+    formidable = require('formidable'),
+    http = require('http'),
+    util = require('util'),
+    os = require("os"),
+    setting = require('./app/setting'),
+    Render = require('./app/render'),
+    logger = require('./logger');
 
 //获取ip地址
 var address
@@ -46,7 +46,8 @@ Object.keys(networks).forEach(function (k) {
         }
     }
 })
-let i = 1;
+
+let i = 0;
 //创建服务器
 let server = http.createServer(function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -55,26 +56,26 @@ let server = http.createServer(function (req, res) {
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    console.log('server  收到客户端的请求数量', req.url, req.method, i++);
+    logger.debug(` server  收到客户端的请求数量`, req.url, req.method, ++i);
     if (req.url === "/" && req.method === "GET") {
         //显示主页
-        res.setHeader('Content-Type', 'text/html;charset=UTF-8')
-        let content = fs.readFileSync("./index.html");
+        res.setHeader('Content-Type', 'text/html;charset=UTF-8');
+        var content = fs.readFileSync("./index.html");
         res.write(content);
         res.end();
     } else if (req.url === "/Images" && req.method === "POST") {
         //上传文件
-        let files = [];
+        var files = [];
         var form = new formidable.IncomingForm();
         form.multiples = true; //q启用多文件上传
         form.maxFileSize = 1 * 1024 * 1024 * 1024; //限制上传最大文件为4GB
         form.on('file', function (filed, file) {
             files.push([filed, file]);
         }).parse(req, function (err, fields, files) {
-            // console.log("fields", fields);
-            //console.log("files", files);
+            // logger.debug("fields", fields);
+            //logger.debug("files", files);
             if (err) {
-                console.log("err" + err.message);
+                logger.debug(`  err` + err.message);
                 return;
             }
             let filesArray = files.files;
@@ -82,7 +83,7 @@ let server = http.createServer(function (req, res) {
             if (Object.prototype.toString.call(files.files) === '[object Object]') {
                 let filesname = files.files.name;
                 let filesize = files.files.size;
-                console.log('上传的是单文件',filesname);
+                logger.info(` 上传的是单文件`, filesname);
                 if (!/\.exe$|\.apk$/gim.test(filesname)) {
                     return res.end("非法类型的文件");
                 } else if (/%|#/g.test(filesname)) {
@@ -94,7 +95,7 @@ let server = http.createServer(function (req, res) {
                     'content-type': 'application/octet-stream'
                 });
                 res.write('received upload:\n\n');
-                //console.log("files", files.files.path);
+                //logger.debug("files", files.files.path);
                 let readStream = fs.createReadStream(files.files.path);
                 let writeStream = fs.createWriteStream("Images/" + filesname);
                 readStream.pipe(writeStream);
@@ -102,9 +103,9 @@ let server = http.createServer(function (req, res) {
                     fs.unlinkSync(files.files.path);
                 });
             } else {
-                console.log('上传的是多文件');
+                logger.debug(`  上传的是多文件`);
                 for (let i = 0; i < filesnum; i++) {
-                    console.log("上传的文件名",filesArray[i].name);
+                    logger.debug(`  上传的文件名`, filesArray[i].name);
                     if (!/\.exe$|\.apk$/gim.test(filesArray[i].name)) {
                         return res.end("非法类型的文件");
                     } else if (/%|#/g.test(filesArray[i].name)) {
@@ -113,7 +114,7 @@ let server = http.createServer(function (req, res) {
                         return res.end('文件大小超过1GB');
                     }
                     res.write('received upload:\n\n');
-                    console.log("files", files.files[i].path);
+                    //logger.debug("files", files.files[i].path);
                     let readStream = fs.createReadStream(files.files[i].path);
                     let writeStream = fs.createWriteStream("Images/" + filesArray[i].name);
                     readStream.pipe(writeStream);
@@ -133,21 +134,22 @@ let server = http.createServer(function (req, res) {
         if (ip.split(',').length > 0) {
             ip = ip.split(',')[0]
         }
-        let time = new Date();
-        console.log(`${time}  访问者ip`, ip);
+        logger.info(` 访问者ip`, ip);
         let content = fs.readdirSync(path.join(__dirname, "Images"));
-        console.log("server  反馈给ajax的请求", content.length);
+        logger.debug(` server  反馈给ajax的请求`, content.length);
         res.write(content.toString());
         res.end();
     } else if (/delete/.test(req.url)) {
         res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
         let pathname = url.parse(req.url).pathname;
         filename = decodeURIComponent(pathname).split("/")[decodeURIComponent(pathname).split("/").length - 1];
-        console.log("server delete filename", filename);
-        if(fs.existsSync(`./Images/${filename}`)){
+        logger.debug(` server delete filename`, filename);
+        if (fs.existsSync(`./Images/${filename}`)) {
             fs.unlink(`./Images/${filename}`, function (err) {
-                if (err) throw err;
-                console.log(`${filename}删除成功!`);
+                if (err) {
+                    throw err
+                }
+                logger.info(`  ${filename}删除成功!`);
                 res.end();
             });
         } else {
@@ -155,7 +157,7 @@ let server = http.createServer(function (req, res) {
         }
     } else {
         //静态文件部署
-        console.log("server  处理静态文件");
+        logger.debug(` server  处理静态文件`);
         const _render = new Render(req, res);
         _render.init();
     }
@@ -166,9 +168,9 @@ server.listen({
     exclusive: true
 });
 server.on('listening', function () {
-    console.log(`服务启动成功,正在监听${setting.port}端口`.green);
+    logger.info(`  服务启动成功,正在监听${setting.port}端口`);
     process.title = `服务启动成功--${address}-${setting.port}`;
 });
 server.on('error', function () {
-    console.log(`${setting.port}端口使用中`.bold.red);
+    logger.warn(`  ${setting.port}端口使用中`.bold.red);
 });
